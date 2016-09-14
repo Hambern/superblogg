9efa85b6f02c9a4fc31d7cb0d350f89db95025a4
Mattias Hamberg <mattias.h@addcream.se>
2016-09-14 12:17:43 +0200

    Vad händer nu?

--- class/acl.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Acl extends Acl_Core {
-
-  protected function categoryAdminAccess($User) {
-    return $User->id() == 1;
-  }
-  
-}
--- view/category/edit.php
+++
@@ -1,6 +0,0 @@
-<?php
-$this->Html->h1 = $this->Html->title = t("Edit category");
-$this->Html->breadcrumbs[] = ["category/list", t("categories")];
-$this->Html->breadcrumbs[] = $this->Html->title;
-
-print $form;
